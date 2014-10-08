7cedd7d7aed4ec2f2c21da4ff32df032fa5e03cf
Ethan Rubinson <ethanrubinson@yahoo.com>
2014-10-08 19:11:44 -0400

    Delete implementation

--- src/edu/cornell/cs4320/hw2/IsamDataNode.java
+++ src/edu/cornell/cs4320/hw2/IsamDataNode.java
@@ -79,13 +79,92 @@ public class IsamDataNode extends IsamNode {
 			return data + "]";
 		}
 	}
-
+	
+	private boolean isEmpty(){
+		for(int key_index = 0; key_index < getSize(); key_index++)
+			if (keys[key_index] != null)
+				return false;
+		
+		return true;
+	}
+	
+	private int doDelete(Integer key, int depth){
+		
+		for(int key_index = 0; key_index < getSize(); key_index++){
+			if (keys[key_index] != null && keys[key_index].intValue() == key.intValue()) {
+				keys[key_index] = null;
+				values[key_index] = null;
+				return depth;
+			}
+		}
+		
+		return getOverflowPage().doDelete(key, depth + 1);
+		
+	}
+	
 	@Override
 	public boolean delete(Integer key) {
-		/*
-		 * TODO: delete a value from this node (or one of its successors)
-		 */
-
-		return false;
+		if(search(key) == null)
+			return false;
+		
+		int depth = doDelete(key, 0);
+		
+		// If there are no overflow pages then no merge is possible
+		if(!hasOverflow())
+			return true;
+		
+		// Get a reference to the page where the element was removed
+		IsamDataNode nodeWithSpace = this;
+		for(int iter = 0; iter < depth; iter++){
+			nodeWithSpace = nodeWithSpace.getOverflowPage();
+		}
+		
+		// Count the total number of overflow pages and a reference to the last page
+		int numOverflowPages = 0;
+		IsamDataNode lastNode = this;
+		IsamDataNode prevLastNode = this;
+		while(lastNode.hasOverflow()){
+			prevLastNode = lastNode;
+			lastNode = lastNode.getOverflowPage();
+			numOverflowPages++;
+		}
+		
+		
+		// CASE 1: The element was removed from the last overflow page
+		if(numOverflowPages == depth) {
+			// If the overflow page is now empty, remove it
+			if(nodeWithSpace.isEmpty()) {
+				lastNode = null;
+				prevLastNode.successor = null;
+			}
+			
+			return true;
+		}
+		
+		// CASE 2: The element was removed from the first page or a middle page
+		Integer movedKey;
+		String movedValue;
+		
+		// Find the last data object on the last page to move and delete it
+		for(int key_index = getSize() - 1; key_index >= 0; key_index--){
+			if (lastNode.keys[key_index] != null) {
+				movedKey = lastNode.keys[key_index];
+				lastNode.keys[key_index] = null;
+				
+				movedValue = lastNode.values[key_index];
+				lastNode.values[key_index] = null;
+				
+				// If the last page is now empty, delete it
+				if(lastNode.isEmpty()){
+					prevLastNode.successor = null;
+				}
+				
+				// Re insert the moved value
+				nodeWithSpace.doInsert(movedKey, movedValue);
+				break;
+			}
+		}
+		
+		return true;
 	}
 }
